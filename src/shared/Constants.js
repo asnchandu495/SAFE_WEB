119010688cd663acf3daa8805ffa17e0aa4e3a54
saravanan <saravanan@SAMLP17050022.sumeru.net>
2021-03-20 11:18:39 +0530

    updated url

--- src/shared/Constants.js
+++ src/shared/Constants.js
@@ -1,11 +1,9 @@
 const Constants = {
-  // baseURL: "https://umdevssap.azurewebsites.net/api",
-  // authURL: "https://authdevssap.azurewebsites.net",
-
   // baseURL: "https://ssm-app-api.azurewebsites.net/api",
   // authURL: "https://ssm-app-ssm-auth.azurewebsites.net",
 
   baseURL: "https://ssap-api.azurewebsites.net/api",
   authURL: "https://ssap-auth.azurewebsites.net",
+  quentionaireURL: "http://ssapsurvey.azurewebsites.net/api",
 };
 export default Constants;
