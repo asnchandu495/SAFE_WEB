6dd6297ff4ea24729b9c054bb9864aa426a9a4dc
saravanan <saravanan@SAMLP17050022.sumeru.net>
2021-04-06 16:54:12 +0530

    changes

--- src/components/questionaires/questionnaireEvaluation.js
+++ src/components/questionaires/questionnaireEvaluation.js
@@ -1,16 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import Breadcrumbs from "@material-ui/core/Breadcrumbs";
-import HomeIcon from "@material-ui/icons/Home";
-import WbSunnyIcon from "@material-ui/icons/WbSunny";
-import Link from "@material-ui/core/Link";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
 import { makeStyles } from "@material-ui/core/styles";
 import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
 import Button from "@material-ui/core/Button";
-import InputLabel from "@material-ui/core/InputLabel";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
 import CancelIcon from "@material-ui/icons/Cancel";
 import Paper from "@material-ui/core/Paper";
@@ -18,25 +12,23 @@ import Tooltip from "@material-ui/core/Tooltip";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
 import InputAdornment from "@material-ui/core/InputAdornment";
-import FormHelperText from "@material-ui/core/FormHelperText";
 import { Link as LinkTo, useParams } from "react-router-dom";
 import ButtonLoadderComponent from "../common/loadder/buttonloadder";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import TextField from "@material-ui/core/TextField";
 import ToasterMessageComponent from "../common/toaster";
-
-import GlobalSettingApiServices from "../../services/globalSettingService";
 import CovidStateApiServices from "../../services/masterDataService";
 import questionaireService from "../../services/questionaireService";
 
-const CovidStateApi = new CovidStateApiServices();
 const useStyles = makeStyles((theme) => ({
   gridDispaly: {
     display: "inline-flex",
   },
 }));
+
 function QuestionnaireEvaluation(props) {
   const classes = useStyles();
+  const CovidStateApi = new CovidStateApiServices();
   const questionaireApiCall = new questionaireService();
   const { id } = useParams();
   const [showLoadder, setshowLoadder] = useState(false);
@@ -90,41 +82,44 @@ function QuestionnaireEvaluation(props) {
       uomTempvalue: "Celsius",
     },
   ];
+
   useEffect(() => {
     Promise.all([
       CovidStateApi.getCOVIDStates(),
       questionaireApiCall.GetEvaluationId(id),
     ])
-
       .then(([covidstateRes, getEvaluationDetails]) => {
-        // .then((covidstateRes) => {
-        console.log(getEvaluationDetails);
         setcovidStatelist(covidstateRes);
-        temperatureConfigForm.positiveConformityScore =
-          getEvaluationDetails.positiveConformityScore;
+        if (getEvaluationDetails) {
+          setTemperatureConfigForm(getEvaluationDetails);
+        }
       })
       .catch((err) => {
         console.log(err);
-        // throw err;
       });
   }, []);
-  const handleInputChange = (e, index) => {
-    const { name, value } = e.target;
-    const list = [...temperatureConfigForm];
-    list[index][name] = value;
-    setTemperatureConfigForm(list);
-  };
 
   function handleChangeInput(e) {
     const { name, value } = e.target;
-    setTemperatureConfigForm((tempsections) => ({
-      ...tempsections,
-      [name]: value,
+    setTemperatureConfigForm((temperatureConfigForm) => ({
+      ...temperatureConfigForm,
+      [name]: parseInt(value),
+    }));
+  }
+
+  function handleChangeCovidStateMain(e, selectedValue) {
+    let thisValue = {
+      id: selectedValue.id,
+      stateName: selectedValue.stateName,
+    };
+    setTemperatureConfigForm((temperatureConfigForm) => ({
+      ...temperatureConfigForm,
+      ["covidState"]: thisValue,
     }));
   }
 
   function handleChangeCovidState(e, value, index) {
-    let thisValue = { id: value.id, state: value.stateName };
+    let thisValue = { id: value.id, stateName: value.stateName };
     const list = {
       ...temperatureConfigForm,
       positiveResponses: [
@@ -159,11 +154,7 @@ function QuestionnaireEvaluation(props) {
     };
     setTemperatureConfigForm(list);
   };
-  // const handleRemoveClick = (index) => {
-  //   const list = [...temperatureConfigForm];
-  //   list.splice(index, 1);
-  //   setTemperatureConfigForm(list);
-  // };
+
   const handleRemoveClick = (j) => {
     const list = { ...temperatureConfigForm };
     list.positiveResponses.splice(j, 1);
@@ -178,18 +169,6 @@ function QuestionnaireEvaluation(props) {
       [name]: value,
     }));
   }
-  // handle click event of the Add button
-  // const handleAddClick = () => {
-  //   setTemperatureConfigForm([
-  //     ...temperatureConfigForm,
-  //     {
-  //       id: Math.random().toString(36).substring(7),
-  //       covidState: "",
-  //       lowerLimit: "",
-  //       upperLimit: "",
-  //     },
-  //   ]);
-  // };
 
   const handleAddClick = (index, j) => {
     const list = { ...temperatureConfigForm };
@@ -213,22 +192,38 @@ function QuestionnaireEvaluation(props) {
     e.preventDefault();
     console.log("data");
     var formData = temperatureConfigForm;
-    console.log(JSON.stringify(formData));
-    questionaireApiCall
-      .AddEvaluationResultForQuestionnaire(formData)
-      .then((response) => {
-        console.log("success");
-        setStateSnackbar(true);
-        setToasterMessage("created evalauation settings.");
-        settoasterServerity("success");
-        setisAlertBoxOpened(false);
-        setTimeout(() => {
-          setshowLoadder(false);
-        }, 6000);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    if (temperatureConfigForm.id != "") {
+      questionaireApiCall
+        .updateEvaluationResultForQuestionnaire(formData)
+        .then((response) => {
+          console.log("success");
+          setStateSnackbar(true);
+          setToasterMessage("created evalauation settings.");
+          settoasterServerity("success");
+          setisAlertBoxOpened(false);
+          setTimeout(() => {
+            setshowLoadder(false);
+          }, 6000);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      questionaireApiCall
+        .AddEvaluationResultForQuestionnaire(formData)
+        .then((response) => {
+          setStateSnackbar(true);
+          setToasterMessage("created evalauation settings.");
+          settoasterServerity("success");
+          setisAlertBoxOpened(false);
+          setTimeout(() => {
+            setshowLoadder(false);
+          }, 6000);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   }
   return (
     <div className="innerpage-container">
@@ -299,21 +294,22 @@ function QuestionnaireEvaluation(props) {
                   Covid state if one or more red flag responses
                 </label>
               </Grid>
-
               <Grid item xs={3}>
                 <FormControl variant="outlined" fullWidth>
                   <Autocomplete
                     id="tags-outlined"
-                    // /options={teamManagers}
                     options={
                       covidStatelist && covidStatelist.length > 0
                         ? covidStatelist
                         : []
                     }
                     getOptionLabel={(option) => option.stateName}
-                    onChange={(e, v) => handleChangeCovidState(e, v)}
-                    // defaultValue={formData.manager ? formData.manager : {}}
-                    // defaultValue={formData.covidState}
+                    onChange={(e, v) => handleChangeCovidStateMain(e, v)}
+                    defaultValue={
+                      temperatureConfigForm.covidState
+                        ? temperatureConfigForm.covidState
+                        : ""
+                    }
                     filterSelectedOptions
                     className="global-input autocomplete-select"
                     renderInput={(params) => (
@@ -348,51 +344,20 @@ function QuestionnaireEvaluation(props) {
                       </Grid>
                       <Grid item xs={2}>
                         <FormControl variant="outlined" fullWidth>
-                          {/* <InputLabel
-                        id="demo-simple-select-outlined-label"
-                        shrink={false}
-                        className="select-label"
-                      >
-                        {formData.covidState == "" ? "Select covid state" : ""}
-                      </InputLabel>
-                      <Select
-                        labelId="demo-simple-select-outlined-label"
-                        id="demo-simple-select-outlined"
-                        name="covidState"
-                        placeholder=""
-                        className="global-input single-select"
-                        InputLabelProps={{ shrink: false }}
-                        value={x.covidState}
-                        onChange={(e) => handleInputChange(e, i)}
-                      >
-                        <MenuItem value="">None</MenuItem>
-                        {covidStatelist
-                          ? covidStatelist.map((cState) => {
-                              return (
-                                <MenuItem value={cState.id}>
-                                  {cState.stateName}
-                                </MenuItem>
-                              );
-                            })
-                          : ""}
-                      </Select> */}
                           <Autocomplete
                             id="tags-outlined"
-                            // /options={teamManagers}
                             options={
                               covidStatelist && covidStatelist.length > 0
                                 ? covidStatelist
                                 : []
                             }
                             getOptionLabel={(option) => option.stateName}
-                            // onChange={(e) => handleInputChange(e, i)}
                             onChange={(e, v) => handleChangeCovidState(e, v, i)}
                             defaultValue={
                               temperatureConfigForm.positiveResponses
                                 ? temperatureConfigForm.positiveResponses
                                 : []
                             }
-                            // defaultValue={formData.manager ? formData.manager : {}}
                             name="covidState"
                             defaultValue={x.covidState}
                             filterSelectedOptions
