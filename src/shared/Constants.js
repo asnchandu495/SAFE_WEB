d682056f639d78fe50c9556595057963f34076e1
saravanan <saravanan@SAMLP17050022.sumeru.net>
2021-04-15 15:31:44 +0530

    update url

--- src/shared/Constants.js
+++ src/shared/Constants.js
@@ -1,6 +1,7 @@
 const Constants = {
   // baseURL: "https://ssm-app-api.azurewebsites.net/api",
   // authURL: "https://ssm-app-ssm-auth.azurewebsites.net",
+  // quentionaireURL: "https://ssm-app-ssm-survey.azurewebsites.net/api",
 
   baseURL: "https://ssap-api.azurewebsites.net/api",
   authURL: "https://ssap-auth.azurewebsites.net",
