a0a575fe9d4df03ae6b186eb5e36a8d26c67d31c
hajira_siddiqua <hajira.a@sumerusolutions.com>
2021-04-23 16:47:24 +0530

    'workflowdesign'

--- src/components/workflow/CreateWorkflow.js
+++ src/components/workflow/CreateWorkflow.js
@@ -1,9 +1,256 @@
 import React from "react";
 
+import { Fragment, useEffect, useState } from "react";
+import Button from "@material-ui/core/Button";
+import Paper from "@material-ui/core/Paper";
+import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
+import Grid from "@material-ui/core/Grid";
+import Breadcrumbs from "@material-ui/core/Breadcrumbs";
+import { Link as LinkTo } from "react-router-dom";
+import { connect } from "react-redux";
+import * as TeamAction from "../../Redux/Action/teamAction";
+import PropTypes from "prop-types";
+import AlertBoxComponent from "../common/alert";
+import ToasterMessageComponent from "../common/toaster";
+import ButtonLoadderComponent from "../common/loadder/buttonloadder";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import TextField from "@material-ui/core/TextField";
+import ComponentLoadderComponent from "../common/loadder/componentloadder";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import UserGroupService from "../../services/userGroupService";
+import CovidStateApiServices from "../../services/masterDataService";
+import FormControl from "@material-ui/core/FormControl";
+
 function CreateWorkflow(props) {
+  const UserGroup = new UserGroupService();
+  const CovidStateApi = new CovidStateApiServices();
+  const [showLoadder, setshowLoadder] = useState(false);
+  const [userGroupList, setuserGroupList] = useState();
+  const [formFieldValidation, setformFieldValidation] = useState({
+    manager: false,
+  });
+  const [componentLoadder, setComponentLoadder] = useState(true);
+  const [isAlertBoxOpened, setisAlertBoxOpened] = useState(false);
+  const [covidStatelist, setcovidStatelist] = useState([]);
+  useEffect(() => {
+    Promise.all([UserGroup.loadUserGroup(), CovidStateApi.getCOVIDStates()])
+      .then(([getUserList, getCovidStates]) => {
+        setuserGroupList(getUserList);
+        setcovidStatelist(getCovidStates);
+        setComponentLoadder(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
-    <div>
-      <h4>Welcome</h4>
+    <div className="innerpage-container">
+      <AlertBoxComponent isAlertBoxOpened={isAlertBoxOpened} />
+      <Breadcrumbs aria-label="breadcrumb" className="global-breadcrumb">
+        <LinkTo
+          color="inherit"
+          href="#"
+          to={`/home/dashboard`}
+          className="inactive"
+        >
+          Home
+        </LinkTo>
+        <LinkTo
+          color="textPrimary"
+          href="#"
+          to={`/teams/allteams`}
+          className="inactive"
+        >
+          Workflow
+        </LinkTo>
+        <LinkTo
+          color="textPrimary"
+          href="#"
+          to={`/teams/allteams`}
+          className="inactive"
+        >
+          Create workflow
+        </LinkTo>
+      </Breadcrumbs>
+      {!componentLoadder ? (
+        <Paper className="main-paper">
+          <ValidatorForm className={`global-form`} onSubmit="#">
+            <Grid container spacing={3}>
+              <Grid item container xs={12}>
+                <Grid item xs={3}>
+                  <label className="input-label ">Work Flow Name</label>
+                </Grid>
+                <Grid item xs={5}>
+                  <TextValidator
+                    variant="outlined"
+                    validators={[
+                      "required",
+                      "matchRegexp:^[a-zA-Z0-9 ]*$",
+                      "matchRegexp:^.{0,50}$",
+                    ]}
+                    errorMessages={[
+                      "Please Worlflow name",
+                      "Special character  not allowed",
+                      "Maximum 50 characters",
+                    ]}
+                    fullWidth
+                    id="name"
+                    placeholder="Worlflow name"
+                    name="name"
+                    autoComplete="name"
+                    // onChange={handleChange}
+                    // value={formData.name}s
+                    autoFocus
+                    InputLabelProps={{ shrink: false }}
+                    className="global-input"
+                  />
+                </Grid>
+              </Grid>
+
+              <Grid item cs={12} container>
+                <Grid item xs={3}>
+                  <label className="">User Group </label>
+                </Grid>
+                <Grid item xs={5}>
+                  <FormControl variant="outlined" fullWidth>
+                    <Autocomplete
+                      id="tags-outlined"
+                      options={
+                        userGroupList && userGroupList.length > 0
+                          ? userGroupList
+                          : []
+                      }
+                      getOptionLabel={(option) => option.groupName}
+                      defaultValue="#"
+                      //   onChange={selectedUser}
+                      filterSelectedOptions
+                      className="global-input autocomplete-select"
+                      renderInput={(params) => (
+                        <TextField
+                          {...params}
+                          variant="outlined"
+                          placeholder="Select usergroup"
+                        />
+                      )}
+                    />{" "}
+                  </FormControl>
+                </Grid>
+              </Grid>
+
+              <Grid item container xs={12}>
+                <Grid item xs={3}>
+                  <label htmlFor="password" className="input-label ">
+                    From State
+                  </label>
+                </Grid>
+                <Grid item xs={5}>
+                  <Autocomplete
+                    id="tags-outlined"
+                    // /options={teamManagers}
+                    options={
+                      covidStatelist && covidStatelist.length > 0
+                        ? covidStatelist
+                        : []
+                    }
+                    getOptionLabel={(option) => option.stateName}
+                    // onChange={(e, v) => handleChangeCovidState(e, v, i)}
+                    // defaultValue={
+                    //   tempsections.covidStates ? tempsections.covidStates : []
+                    // }
+                    name="covidState"
+                    // defaultValue={x.covidState}
+                    filterSelectedOptions
+                    className="global-input autocomplete-select"
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        variant="outlined"
+                        placeholder="Select   state"
+                      />
+                    )}
+                  />
+                  {formFieldValidation.manager ? (
+                    <FormHelperText className="error-msg">
+                      Please select from state{" "}
+                    </FormHelperText>
+                  ) : (
+                    ""
+                  )}
+                </Grid>
+              </Grid>
+
+              <Grid item container xs={12}>
+                <Grid item xs={3}>
+                  <label htmlFor="password" className="input-label ">
+                    To State
+                  </label>
+                </Grid>
+                <Grid item xs={5}>
+                  <Autocomplete
+                    id="tags-outlined"
+                    // /options={teamManagers}
+                    options={
+                      covidStatelist && covidStatelist.length > 0
+                        ? covidStatelist
+                        : []
+                    }
+                    getOptionLabel={(option) => option.stateName}
+                    // onChange={(e, v) => handleChangeCovidState(e, v, i)}
+                    // defaultValue={
+                    //   tempsections.covidStates ? tempsections.covidStates : []
+                    // }
+                    name="covidState"
+                    // defaultValue={x.covidState}
+                    filterSelectedOptions
+                    className="global-input autocomplete-select"
+                    renderInput={(params) => (
+                      <TextField
+                        {...params}
+                        variant="outlined"
+                        placeholder="Select   state"
+                      />
+                    )}
+                  />
+                  {formFieldValidation.manager ? (
+                    <FormHelperText className="error-msg">
+                      Please select To state{" "}
+                    </FormHelperText>
+                  ) : (
+                    ""
+                  )}
+                </Grid>
+              </Grid>
+              <Grid item container xs={12}>
+                <Grid item xs={3}>
+                  <label>&nbsp;</label>
+                </Grid>
+                <Grid item xs={9}>
+                  <div className={`form-buttons-container`}>
+                    <Button
+                      variant="contained"
+                      type="submit"
+                      className="global-submit-btn"
+                      disabled={showLoadder}
+                    >
+                      {showLoadder ? <ButtonLoadderComponent /> : "Submit"}
+                    </Button>
+                    <Button
+                      variant="contained"
+                      type="reset"
+                      onClick="#"
+                      className="global-cancel-btn"
+                    >
+                      Cancel
+                    </Button>
+                  </div>
+                </Grid>
+              </Grid>
+            </Grid>
+          </ValidatorForm>
+        </Paper>
+      ) : (
+        <ComponentLoadderComponent />
+      )}
     </div>
   );
 }
--- src/components/workflow/index.js
+++ src/components/workflow/index.js
@@ -12,42 +12,58 @@ import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
 import VisibilityIcon from "@material-ui/icons/Visibility";
 import GroupAddIcon from "@material-ui/icons/GroupAdd";
+import BackupIcon from "@material-ui/icons/Backup";
+import AddBoxIcon from "@material-ui/icons/AddBox";
+import AddIcon from "@material-ui/icons/Add";
+import FilterListIcon from "@material-ui/icons/FilterList";
+import UserGroupService from "../../services/userGroupService";
+import ConfirmationDialog from "../common/confirmdialogbox";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import ComponentLoadderComponent from "../common/loadder/componentloadder";
+import ButtonLoadderComponent from "../common/loadder/buttonloadder";
+import ToasterMessageComponent from "../common/toaster";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 
 function Workflow(props) {
+  const UserGroup = new UserGroupService();
   const [responsive, setResponsive] = useState("vertical");
   const [tableBodyHeight, setTableBodyHeight] = useState("300px");
   const [tableBodyMaxHeight, setTableBodyMaxHeight] = useState("");
+  const [userGroupList, setuserGroupList] = useState();
+  const [componentLoadder, setComponentLoadder] = useState(true);
+  const [Modalopen, setModalOpen] = useState(false);
+  const [showLoadder, setshowLoadder] = useState(false);
+  const userStatusData = [
+    { id: true, name: "Active" },
+    { id: false, name: "Inactive" },
+  ];
   const columns = [
     {
-      label: "Name ",
+      label: "Work Flow Name ",
       options: {
         filter: false,
         sort: true,
       },
     },
     {
-      label: "Title",
+      label: "User Group",
       options: {
         filter: false,
         sort: true,
       },
     },
     {
-      label: "Location",
-      options: {
-        filter: false,
-        sort: true,
-      },
-    },
-    {
-      label: "Age",
-      options: {
-        filter: false,
-        sort: true,
-      },
-    },
-    {
-      label: "Salary",
+      label: "Status",
       options: {
         filter: false,
         sort: true,
@@ -84,6 +100,16 @@ function Workflow(props) {
                   ></Button>
                 </Tooltip>
 
+                <Tooltip title="Add">
+                  <Button
+                    variant="contained"
+                    color="default"
+                    startIcon={<AddIcon />}
+                    className={`edit-icon`}
+                    onClick="#"
+                  ></Button>
+                </Tooltip>
+
                 <Tooltip title="Delete">
                   <Button
                     variant="contained"
@@ -94,11 +120,11 @@ function Workflow(props) {
                   ></Button>
                 </Tooltip>
 
-                <Tooltip title="Assign users to team">
+                <Tooltip title="Upload">
                   <Button
                     variant="contained"
                     color="default"
-                    startIcon={<GroupAddIcon />}
+                    startIcon={<BackupIcon />}
                     className={`edit-icon`}
                     onClick="#"
                   ></Button>
@@ -107,6 +133,7 @@ function Workflow(props) {
             );
           }
         },
+
         setCellProps: (value) => {
           return {
             style: { width: "250px", minWidth: "250px", textAlign: "center" },
@@ -121,8 +148,8 @@ function Workflow(props) {
     filterType: "dropdown",
     responsive: "scroll",
     fixedHeader: true,
-    // rowsPerPageOptions: [5, 10, 15, 100],
-    // rowsPerPage: 5,
+    rowsPerPageOptions: [5, 10, 15, 100],
+    rowsPerPage: 5,
     print: false,
     viewColumns: false,
     download: false,
@@ -132,43 +159,166 @@ function Workflow(props) {
         noMatch: "There are no users  teams",
       },
     },
+    customToolbarSelect: (value, tableMeta, updateValue) => {},
+    customToolbar: () => {
+      return (
+        <div className={`maingrid-actions`}>
+          <Tooltip title="Filter By User">
+            <Button
+              variant="contained"
+              startIcon={<FilterListIcon />}
+              className={`add-icon`}
+              onClick={handleClickOpenModal}
+            ></Button>
+          </Tooltip>
+        </div>
+      );
+    },
   };
 
   let data = [
-    ["Harper White", "Attorney", "Pittsburgh", 52, 420000],
-    ["Kris Humphrey", "Agency Legal Counsel", "Laredo", 30, 150000],
-    ["Frankie Long", "Industrial Analyst", "Austin", 31, 170000],
-    ["Brynn Robbins", "Business Analyst", "Norfolk", 22, 90000],
-    ["Justice Mann", "Business Consultant", "Chicago", 24, 133000],
-    ["Addison Navarro", "Business Management Analyst", "New York", 50, 295000],
-    ["Jesse Welch", "Agency Legal Counsel", "Seattle", 28, 200000],
-    ["Eli Mejia", "Commercial Specialist", "Long Beach", 65, 400000],
-    ["Gene Leblanc", "Industrial Analyst", "Hartford", 34, 110000],
-    ["Danny Leon", "Computer Scientist", "Newark", 60, 220000],
-    ["Lane Lee", "Corporate Counselor", "Cincinnati", 52, 180000],
-    ["Jesse Hall", "Business Analyst", "Baltimore", 44, 99000],
-    ["Danni Hudson", "Agency Legal Counsel", "Tampa", 37, 90000],
-    ["Terry Macdonald", "Commercial Specialist", "Miami", 39, 140000],
-    ["Justice Mccarthy", "Attorney", "Tucson", 26, 330000],
-    ["Silver Carey", "Computer Scientist", "Memphis", 47, 250000],
-    ["Franky Miles", "Industrial Analyst", "Buffalo", 49, 190000],
-    ["Glen Nixon", "Corporate Counselor", "Arlington", 44, 80000],
-    [
-      "Gabby Strickland",
-      "Business Process Consultant",
-      "Scottsdale",
-      26,
-      45000,
-    ],
-    ["Mason Ray", "Computer Scientist", "San Francisco", 39, 142000],
+    ["Harper White", "Attorney", "Pittsburgh"],
+    ["Kris Humphrey", "Agency Legal Counsel", "Laredo"],
+    ["Frankie Long", "Industrial Analyst", "Austin"],
+    ["Brynn Robbins", "Business Analyst", "Norfolk"],
+    ["Justice Mann", "Business Consultant", "Chicago"],
+    ["Addison Navarro", "Business Management Analyst", "New York"],
+    ["Jesse Welch", "Agency Legal Counsel", "Seattle"],
+    ["Eli Mejia", "Commercial Specialist", "Long Beach"],
+    ["Gene Leblanc", "Industrial Analyst", "Hartford"],
+    ["Danny Leon", "Computer Scientist", "Newark"],
+    ["Lane Lee", "Corporate Counselor", "Cincinnati"],
+    ["Jesse Hall", "Business Analyst", "Baltimore"],
+    ["Danni Hudson", "Agency Legal Counsel", "Tampa"],
+    ["Terry Macdonald", "Commercial Specialist", "Miami"],
+    ["Justice Mccarthy", "Attorney", "Tucson"],
+    ["Silver Carey", "Computer Scientist", "Memphis"],
+    ["Franky Miles", "Industrial Analyst", "Buffalo"],
+    ["Glen Nixon", "Corporate Counselor", "Arlington"],
+    ["Gabby Strickland", "Business Process Consultant", "Scottsdale"],
+    ["Mason Ray", "Computer Scientist", "San Francisco"],
   ];
 
   function BreadcrumbNavigation(getRoute) {
     props.history.push(getRoute);
   }
 
+  const handleClickOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleClose = () => {
+    setModalOpen(false);
+  };
+
+  useEffect(() => {
+    UserGroup.loadUserGroup()
+      .then((getUsergrouplist) => {
+        setuserGroupList(getUsergrouplist);
+        setComponentLoadder(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div className="innerpage-container">
+      <Dialog
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+        open={Modalopen}
+      >
+        <DialogTitle id="form-dialog-title" onClose={handleClose}>
+          Filters
+        </DialogTitle>
+        <ValidatorForm className={`global-form`} onSubmit="#">
+          <DialogContent dividers>
+            {!componentLoadder ? (
+              <Grid container spacing={3}>
+                <Grid item cs={12} container>
+                  <Grid item xs={4}>
+                    <label className="">User Group </label>
+                  </Grid>
+                  <Grid item xs={8}>
+                    <FormControl variant="outlined" fullWidth>
+                      <Autocomplete
+                        id="tags-outlined"
+                        options={
+                          userGroupList && userGroupList.length > 0
+                            ? userGroupList
+                            : []
+                        }
+                        getOptionLabel={(option) => option.groupName}
+                        // defaultValue="#"
+                        // onChange="#"
+                        filterSelectedOptions
+                        className="global-input autocomplete-select"
+                        renderInput={(params) => (
+                          <TextField
+                            {...params}
+                            variant="outlined"
+                            placeholder="Select usergroup"
+                          />
+                        )}
+                      />{" "}
+                    </FormControl>
+                  </Grid>
+                </Grid>
+                <Grid item xs={12} container>
+                  <Grid item xs={4}>
+                    <label className="">Status</label>
+                  </Grid>
+                  <Grid item xs={8}>
+                    <FormControl variant="outlined" fullWidth>
+                      <InputLabel
+                        id="demo-simple-select-outlined-label"
+                        shrink={false}
+                        className="select-label"
+                      >
+                        {/* {formData.isActive != "" ? "Select status" : ""} */}
+                      </InputLabel>
+                      <Select
+                        labelId="demo-simple-select-outlined-label"
+                        id="demo-simple-select-outlined"
+                        placeholder="Select status"
+                        name="isActive"
+                        // value={formData.isActive}
+                        // onChange={handleChange}
+                        className="global-input single-select"
+                      >
+                        <MenuItem value="">None</MenuItem>
+                        {userStatusData.length > 0
+                          ? userStatusData.map((UserStatus) => {
+                              return (
+                                <MenuItem value={UserStatus.id}>
+                                  {UserStatus.name}
+                                </MenuItem>
+                              );
+                            })
+                          : ""}
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                </Grid>
+              </Grid>
+            ) : null}
+          </DialogContent>
+          <DialogActions>
+            <Button
+              variant="contained"
+              type="submit"
+              className="global-submit-btn"
+              disabled={showLoadder}
+            >
+              {showLoadder ? <ButtonLoadderComponent /> : "Submit"}
+            </Button>
+            <Button onClick={handleClose} className="global-cancel-btn">
+              Cancel
+            </Button>
+          </DialogActions>
+        </ValidatorForm>
+      </Dialog>
+
       <Breadcrumbs aria-label="breadcrumb" className="global-breadcrumb">
         <LinkTo
           color="inherit"
@@ -179,7 +329,10 @@ function Workflow(props) {
           Home
         </LinkTo>
         <LinkTo color="textPrimary" href="#" to="#" className="inactive">
-          workflow
+          Work Flow
+        </LinkTo>
+        <LinkTo color="textPrimary" href="#" to="#" className="inactive">
+          List Work Flow
         </LinkTo>
       </Breadcrumbs>
 
