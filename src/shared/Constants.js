540d9e52c6bc80ffdbbb0768c0a567154c7c56d9
hajira_siddiqua <hajira.a@sumerusolutions.com>
2021-07-23 19:20:33 +0530

    endpoints

--- src/shared/Constants.js
+++ src/shared/Constants.js
@@ -1,10 +1,17 @@
 const Constants = {
+  //UAT
   // baseURL: "https://ssm-app-api.azurewebsites.net/api",
   // authURL: "https://ssm-app-ssm-auth.azurewebsites.net",
   // quentionaireURL: "https://ssm-app-ssm-survey.azurewebsites.net/api",
 
+  //QA
   baseURL: "https://ssap-api.azurewebsites.net/api",
   authURL: "https://ssap-auth.azurewebsites.net",
   quentionaireURL: "https://ssapsurvey.azurewebsites.net/api",
+
+  //PRODUCTION
+  // baseURL: "https://127.0.0.1:5001/api",
+  // authURL: "https://ssm-app-ssm-auth.azurewebsites.net",
+  // quentionaireURL: "https://ssm-app-ssm-survey.azurewebsites.net/api",
 };
 export default Constants;
