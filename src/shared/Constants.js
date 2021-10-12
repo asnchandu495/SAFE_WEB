e9238aeec4f9005199bb250891f2ae648273e7dc
hajira siddiqua <hajira.a@sumerusolutions.com>
2021-10-12 17:36:11 +0530

    fixes

--- src/shared/Constants.js
+++ src/shared/Constants.js
@@ -1,13 +1,13 @@
 const Constants = {
   //UAT
-  // baseURL: "https://ssm-app-api.azurewebsites.net/api",
-  // authURL: "https://ssm-app-ssm-auth.azurewebsites.net",
-  // quentionaireURL: "https://ssm-app-ssm-survey.azurewebsites.net/api",
+  baseURL: "https://ssm-app-api.azurewebsites.net/api",
+  authURL: "https://ssm-app-ssm-auth.azurewebsites.net",
+  quentionaireURL: "https://ssm-app-ssm-survey.azurewebsites.net/api",
 
   //QA
-  baseURL: "https://ssap-api.azurewebsites.net/api",
-  authURL: "https://ssap-auth.azurewebsites.net",
-  quentionaireURL: "https://ssapsurvey.azurewebsites.net/api",
+  // baseURL: "https://ssap-api.azurewebsites.net/api",
+  // authURL: "https://ssap-auth.azurewebsites.net",
+  // quentionaireURL: "https://ssapsurvey.azurewebsites.net/api",
 
   //PRODUCTION
   // baseURL: "https://127.0.0.1:5001/api",
